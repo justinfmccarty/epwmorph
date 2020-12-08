d46102782f37b4bad7837ecf9310d1beb54553e5
Justin <mccarty.justin.f@gmail.com>
2020-12-07 23:43:57 -0800

    Add config file

--- morpher/config.py
+++ morpher/config.py
@@ -4,8 +4,8 @@ Extracts details from the configuration file for guiding the morphing script.
 """
 
 # imports
-
-
+import configparser
+import os
 
 __author__ = "Justin McCarty"
 __copyright__ = "Copyright 2020, justinmccarty"
@@ -16,11 +16,13 @@ __maintainer__ = "Justin McCarty"
 __email__ = "mccarty.justin.f@gmail.com"
 __status__ = "Production"
 
+# set universal variables
+config_file = os.path.join(os.path.dirname(__file__), 'default.config')
 
-
-
-def main(x):
-    print(x)
+def parse(setting):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config["settings"][setting]
 
 if __name__ == '__main__':
-    main(5)
+    parse('one')
---
+++ morpher/morpher_main.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""
+The primary moprhing script.
+"""
+
+# imports
+import morpher.configparser
+import os
+
+__author__ = "Justin McCarty"
+__copyright__ = "Copyright 2020, justinmccarty"
+__credits__ = ["Justin McCarty"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Justin McCarty"
+__email__ = "mccarty.justin.f@gmail.com"
+__status__ = "Production"
+
+
+
+
+
+if __name__ == '__main__':
+    main()
